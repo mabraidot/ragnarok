9d323a044e025ebade778c9b4e2887e7eb5267f8
mabraidot <mabraidot@gmail.com>
2019-10-31 21:21:45 -0300

    Created a basic react app for the front-end

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,21 @@
 from app import app
+from flask import jsonify
 
-@app.route("/")
-@app.route("/index")
+app.config['JSON_AS_ASCII'] = False
+
+
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
 def home():
-    return "The Ragnarök is coming ..."
+    return jsonify(response='The Ragnarök is coming ...')
+
+
+@app.route('/mashtun', methods=['GET'])
+@app.route('/mashtun/get/temperature', methods=['GET'])
+def getMashTunTemperature():
+    return '34'
+
+
+@app.route('/mashtun/set/temperature/<int:degrees>', methods=['POST'])
+def setMashTunTemperature(degrees):
+    return str(degrees)
