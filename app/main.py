e3d209f6e3250b62b4868f34e5bcaa8d0b945916
mabraidot <mabraidot@gmail.com>
2019-11-02 19:20:37 -0300

    cleanup

--- app/main.py
+++ app/main.py
@@ -1,11 +1,6 @@
 from aiohttp import web
 import app.routes as routes
 
-async def handle(request):
-    name = request.match_info.get('name', "Anonymous")
-    text = "Helloooo, " + name
-    return web.Response(text=text)
-
 app = web.Application()
 routes.setup_routes(app)
 
