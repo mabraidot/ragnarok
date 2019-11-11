d7d177946f551da9c3687ff94aa8616004b64ca6
mabraidot <mabraidot@gmail.com>
2019-11-10 22:12:53 -0300

    Added documentation for cooking steps definition

--- app/cooking.py
+++ app/cooking.py
@@ -1,4 +1,36 @@
 
+"""
+BeerXML Format
+==============
+RECIPE
+------
+* BOIL_TIME: duration of boiling expressed in minutes
+
+MASH STEP
+---------
+* INFUSE_AMOUNT: hot water to add at the start of the step. If it's zero, is a temperature ramp for saccharification.
+* INFUSE_TEMP: temperature to pre-heat the infused water (format '00.0 C'. Transform to number).
+* STEP_TIME: duration of the step after water reach the target step temperature.
+* STEP_TEMP: target temperature for the step.
+Optional:
+* RAMP_TIME: it's an estimated time for the mash to reach the target temperature, based on equipment's hardware.
+
+HOP
+---
+* TIME: duration of the hop infusion/boiling expressed in minutes, as remaining time of the full mash or boil process.
+* USE: step in wich the hop is going to be added, can be 'Mash', 'First Wort', 'Boil', 'Dry Hop' or 'Aroma' (on whirlpool)
+Optional:
+* AMOUNT: weight of the addition expressed in kilograms.
+
+MISC
+----
+* TIME: duration of the hop infusion/boiling expressed in minutes, as remaining time of the full mash or boil process.
+* USE: step in wich the hop is going to be added, can be 'Mash', 'Boil', 'Primary', 'Secondary' or 'Bottling'
+Optional:
+* TYPE: can be 'Fining', 'Water Agent', 'Spice', 'Herb', 'Flavor' or 'Other'
+* AMOUNT: weight of the addition expressed in kilograms.
+
+"""
 class Cooking:
     def __init__(self, app):
         self.app = app
