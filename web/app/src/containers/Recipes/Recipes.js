8d60b42ce0a6c55bbf3b48f7aedc9f9c9c81a034
mabraidot <mabraidot@gmail.com>
2019-12-04 23:53:09 -0300

    WIP: added import xml button to recipes

--- web/app/src/containers/Recipes/Recipes.js
+++ web/app/src/containers/Recipes/Recipes.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
 import './Recipes.scss';
 import Grow from '@material-ui/core/Grow';
+import Button from '@material-ui/core/Button';
 
 class Recipes extends Component {
+
+  constructor(props) {
+    super(props);
+      this.state = {
+        importedFile: null
+      }
+      this.fileUploadHandler = this.fileUploadHandler.bind(this);
+  }
+
+  fileUploadHandler(event){
+    this.setState({
+      importedFile: event.target.files[0],
+    });
+    console.log('[RECIPES] File upload: ', event.target.files[0]);
+  }
 
   render() {
     return(
       <Grow in={true}>
         <div className="Recipes">
           <h1>Recipes</h1>
-          <p>
-            Recipes' page
-          </p>
+          <div>
+            <Button
+              variant="contained"
+              component="label"
+              size="large" 
+              className="button-upload"
+            >
+              Import new Recipe
+              <input
+                type="file"
+                onChange={this.fileUploadHandler}
+                style={{ display: "none" }}
+              />
+            </Button>
+          </div>
         </div>
       </Grow>
     );
