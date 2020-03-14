d0d0f2ffed771449ae878333979a757e38c96920
mabraidot <mabraidot@gmail.com>
2020-03-14 14:56:36 -0300

    Added Websocket reconnection on error

--- web/app/src/components/Socket/Socket.js
+++ web/app/src/components/Socket/Socket.js
@@ -2,6 +2,11 @@ const BASE_URI = `ws://${window.location.hostname}:8000/ws`;
 
 class Socket {
   constructor() {
+    this.socket = null;
+    this.open();
+  }
+
+  open() {
     this.socket = new WebSocket(BASE_URI)
 
     this.socket.onopen = () => {
@@ -11,12 +16,20 @@ class Socket {
     this.socket.onerror = (error) => {
       console.log('[WS]: error!:', error.message);
       this.socket.close();
+      this.reconnect();
     };
     
     this.socket.onclose = (close) => {
       console.log('[WS]: Closing:', close.code, close.reason);
     };
+  }
 
+  reconnect() {
+    var that = this;
+    setTimeout(function(){
+      console.log("[WS]: reconnecting ...");
+      that.open();
+    },2000);
   }
 
 }
