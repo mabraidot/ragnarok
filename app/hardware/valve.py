013d5019103d4f3e9fe5421d7482850504cab9c3
mabraidot <mabraidot@gmail.com>
2020-03-31 01:11:49 -0300

    Slowed down servo rotation

--- app/hardware/valve.py
+++ app/hardware/valve.py
@@ -15,28 +15,26 @@ class valve:
         return self.value
     
     def set(self, newValue = 0):
+        direction = 1
+        previousValue = self.value
+        if int(newValue) < previousValue:
+            direction = -1
+
         self.value = int(newValue)
         if self.config.get('DEFAULT', 'ENVIRONMENT') == 'production':
-            # task = threading.Thread(target=self.run)
-            # task.start()
             # newValue: percentage, 100 % = 90 servo degrees
             GPIO.output(self.enablePin, GPIO.LOW)
             sleep(0.2)
-            self.app.servoKit.servo[self.channel].angle = int(self.value * 0.90)
-            sleep(1)
+
+            # self.app.servoKit.servo[self.channel].angle = int(self.value * 0.90)
+            # sleep(1)
+            startPos = int(previousValue * 0.90)
+            endPos = int(self.value * 0.90) + (1 * direction)
+            for i in range(startPos, endPos, 1 * direction):
+                self.app.servoKit.servo[self.channel].angle = int(i)
+                sleep(0.02)
+            
             self.app.servoKit.servo[self.channel].angle = None
             self.app.servoKit.servo[self.channel].fraction = None
             GPIO.output(self.enablePin, GPIO.HIGH)
             sleep(0.2)
-            
-
-    # def run(self):
-    #     # newValue: percentage, 100 % = 90 servo degrees
-    #     # GPIO.output(self.enablePin, GPIO.LOW)
-    #     # sleep(0.2)
-    #     self.app.servoKit.servo[self.channel].angle = int(self.value * 0.90)
-    #     sleep(1)
-    #     self.app.servoKit.servo[self.channel].angle = None
-    #     self.app.servoKit.servo[self.channel].fraction = None
-    #     # GPIO.output(self.enablePin, GPIO.HIGH)
-    #     # sleep(0.2)
